91365c03803385936664912a3d6a83d94218a708
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-11-13 23:15:17 +0100

    Implemented one observer

---
+++ observer/src/DisplayConditions.java
@@ -0,0 +1,25 @@
+public class DisplayConditions implements Observer, IDisplay {
+
+    private float temperature, humidity, pressure;
+    private Subject weatherBase;
+
+    public DisplayConditions(Subject subject) {
+        weatherBase = subject;
+        subject.AddObserver(this);
+    }
+
+    @Override
+    public void Update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        Display();
+    }
+
+    @Override
+    public void Display() {
+        System.out.println("Temp : " + temperature);
+        System.out.println("Humidity : " + humidity);
+        System.out.println("Pressure : " + pressure);
+    }
+}
